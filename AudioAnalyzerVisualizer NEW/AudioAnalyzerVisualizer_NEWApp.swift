c79de5ae02c12e75c404dd7a4ee8b5fb184eb946
maksymdyuh <144259634+maksymdyuh@users.noreply.github.com>
2025-09-16 18:40:35 +0200

    Initial Commit

---
+++ AudioAnalyzerVisualizer NEW/AudioAnalyzerVisualizer_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAnalyzerVisualizer_NEWApp.swift
+//  AudioAnalyzerVisualizer NEW
+//
+//  Created by Максим Дюг on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioAnalyzerVisualizer_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
